ca05e9339a7a7e8e5d16fec70eedfdbf970bee09
rayssinhamilgrau123 <115225506+rayssinhamilgrau123@users.noreply.github.com>
2024-08-16 12:34:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script src='script.js'></script>
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultado');
+const caixaResultados = document.querySelector('.caixa-resultado');
